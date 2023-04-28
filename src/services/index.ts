db90a8f4913f7ba75283185fd7ae1c93aa30bb8b
Võ Hoàng Vũ <vuprotk21@gmail.com>
2023-04-29 00:08:26 +0700

    fix: loading env

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
-const ROOT_DOMAIN = process.env.VITE_API_HOST;
+const ROOT_DOMAIN = import.meta.env.VITE_API_HOST;
 export function generateUrlByService(service, path){
     return `https://${service}.${ROOT_DOMAIN}/api/${path}`;
 }
